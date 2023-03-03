49d123aed57950f372dd257ce57110899f78ad74
theBF <foxaudioresearch@gmail.com>
2023-03-02 19:05:50 -0500

    Updates

--- demo/do-prime-asm-code.asm
+++ demo/do-prime-asm-code.asm
@@ -1,13 +1,14 @@
-   DAEA  0646  dect R6                     (14)
-   DAEC  C584  mov  R4,*R6                 (30)
-   DAEE  0204  li   R4,>2000               (20)
-   DAF2  0646  dect R6                     (14)
-   DAF4  C584  mov  R4,*R6                 (30)
-   DAF6  0204  li   R4,>1ffe               (20)
-   DAFA  0646  dect R6                     (14)
-   DAFC  C584  mov  R4,*R6                 (30)
-   DAFE  0204  li   R4,>0101               (20)
-   DB02  06A0  bl   @>dac0                 (32)  * CALL FILLW 
+   DAEA  0646  dect R6             * parameters onto DATA stack         
+   DAEC  C584  mov  R4,*R6                 
+   DAEE  0204  li   R4,>2000              
+   DAF2  0646  dect R6                    
+   DAF4  C584  mov  R4,*R6                 
+   DAF6  0204  li   R4,>1ffe              
+   DAFA  0646  dect R6                    
+   DAFC  C584  mov  R4,*R6              
+   DAFE  0204  li   R4,>0101              
+   DB02  06A0  bl   @>dac0          * CALL FILLW 
+
   *************** sieve program *****************
    DB06  04C0  clr  R0                     (14)
    DB08  04C3  clr  R3                     (14)
@@ -16,11 +17,11 @@
    DB10  C584  mov  R4,*R6                 (30)
    DB12  0204  li   R4,>0000               (20)
 >  DB16  0646  dect R6                    
-   DB18  C584  mov  R4,*R6                
+   DB18  C584  mov  R4,*R6         * loop index on DATA stack        
    DB1A  0204  li   R4,>1ffe              
-   DB1E  0647  dect R7             * FOR loop starts        
+   DB1E  0647  dect R7             * FOR loop push TOS cache onto return stack 
    DB20  C5C4  mov  R4,*R7                
-   DB22  C136  mov  *R6+,R4               
+   DB22  C136  mov  *R6+,R4        * refill TOS cache from DATA stack        
    DB24  90F5  cb   *R5+,R3               
    DB26  130E  jeq  >db44                 
    DB28  C040  mov  R0,R1                 
